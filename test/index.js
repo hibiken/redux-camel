17ac0a8eb12bc6952c14ea4a1a807d951f343644
Ken Hibino <ken.hibino7@gmail.com>
2017-05-16 21:21:01 -0700

    Add chai, mocha for testing

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const camelMiddleware = require('../src/index');
+
+describe('redux-camel', () => {
+  it('should work', () => {
+    expect(true).to.be.true;
+  });
+});
